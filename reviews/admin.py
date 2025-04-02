aec912f884137d93069b3a0d8aecb41500c2fd74
saripalli <venusaaripalli69@gmail.com>
2025-04-02 23:57:55 +0530

    Added Review model to admin panel

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('name', 'review_text', 'created_at')
+    search_fields = ('name', 'review_text')
+    list_filter = ('created_at',)
+
