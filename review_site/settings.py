aa6b45bda618979f6daf9c3b92b5c3ed2c8937fe
saripalli <venusaaripalli69@gmail.com>
2025-04-03 00:12:11 +0530

    Added Render URL to ALLOWED_HOSTS

--- review_site/settings.py
+++ review_site/settings.py
@@ -124,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+print("DATABASE_URL:", os.getenv("DATABASE_URL"))
